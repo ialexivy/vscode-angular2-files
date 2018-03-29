96d895d4a4ffb3593d82ff403bedc23748be0557
qwert789 <qwert789@gmail.com>
2018-03-29 15:12:30 +0300

    Refactoring default config

--- src/config/cli-config.ts
+++ src/config/cli-config.ts
@@ -1,45 +1,45 @@
-import { IConfig } from "../models/config";
+import { IConfig } from './../models/config';
 
 export const config: IConfig = {
-    "apps": [{
-        "root": "src",
-        "prefix": "app"
-    }],
-    "defaults": {
-        "styleExt": "css",
-        "component": {
-            "spec": true,
-            "inlineStyle": false,
-            "inlineTemplate": false,
-            "flat": false,
-            "changeDetection": "Default",
-            "viewEncapsulation": "Emulated"
-        },
-        "class": {
-            "spec": false
-        }, 
-        "directive": {
-            "flat": true,
-            "spec": true
-        },
-        "guard": {
-            "flat": true,
-            "spec": true
-        },
-        "interface": {
-            "prefix": ""
-        },
-        "module": {
-            "flat": false,
-            "spec": false
-        },
-        "pipe": {
-            "flat": true,
-            "spec": true
-        },
-        "service": {
-            "flat": true,
-            "spec": true
-        }
-    }
-}
+  apps: [{
+    root: 'src',
+    prefix: 'app',
+  }],
+  defaults: {
+    styleExt: 'css',
+    component: {
+      spec: true,
+      inlineStyle: false,
+      inlineTemplate: false,
+      flat: false,
+      changeDetection: 'Default',
+      viewEncapsulation: 'Emulated',
+    },
+    class: {
+      spec: false,
+    },
+    directive: {
+      flat: true,
+      spec: true,
+    },
+    guard: {
+      flat: true,
+      spec: true,
+    },
+    interface: {
+      prefix: '',
+    },
+    module: {
+      flat: false,
+      spec: false,
+    },
+    pipe: {
+      flat: true,
+      spec: true,
+    },
+    service: {
+      flat: true,
+      spec: true,
+    },
+  },
+};
